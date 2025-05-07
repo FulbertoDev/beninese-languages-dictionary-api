ed6751824cba40ed4224986ce54d3fc953e031ac
Fulberto Gilles-Christ TCHIAKPE <fulbertokam@gmail.com>
2025-05-07 21:17:16 +0100

    fix(app): API Docs

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,6 +24,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         JsonResource::withoutWrapping();
+        Scramble::ignoreDefaultRoutes();
         Scramble::configure()
             ->routes(function (Route $route) {
                 return Str::startsWith($route->uri, 'api/');
