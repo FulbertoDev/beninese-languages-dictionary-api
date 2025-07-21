52c579797b778b590ef540dc70cb5bb6e5a338a3
Fulberto Gilles-Christ TCHIAKPE <fulbertokam@gmail.com>
2025-07-21 17:04:21 +0100

    feat(app): Pending

--- app/Http/Controllers/Api/InstallationController.php
+++ app/Http/Controllers/Api/InstallationController.php
@@ -35,8 +35,9 @@ class InstallationController extends Controller
 
     public function checkDeviceSubscription(Request $request, string $id)
     {
-        $headers = $request->headers->all();
         $userAgent = $request->header('user-agent');
+        Log::info("Uset AGent=>" . $userAgent);
+
         if ($userAgent[0] != "Dart 3.8 (dart:io)") {
             Log::info("Not from device");
             return response()->json(null, 400);
